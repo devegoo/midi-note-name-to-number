df57888f51cc12d92bc2196b66990b5ec3f21673
devegoo <57900186+devegoo@users.noreply.github.com>
2020-09-10 12:32:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /opt/nametonote
-cp opt/nametonote/nametonote.py
-cp usr/bin/nametonote /usr/bin/
+cp opt/nametonote/nametonote.py /opt/nametonote/nametonote.py
+cp usr/bin/nametonote /usr/bin/nametonote
 chmod +x /usr/bin/nametonote
